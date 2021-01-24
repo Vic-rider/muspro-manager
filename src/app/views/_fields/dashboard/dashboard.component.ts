a483d732aa9c86b60ff6eeaba04da0e8ef995b80
Vic-rider <tossouvincentsonagnon@gmail.com>
2021-01-24 23:25:05 +0100

    add pie chart, dough chart, more stats

--- src/app/views/_fields/dashboard/dashboard.component.ts
+++ src/app/views/_fields/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js'
 
 @Component({
   selector: 'app-dashboard',
@@ -10,6 +11,65 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.displayUsersPercent()
+    this.displayDistrict()
+  }
+
+  displayUsersPercent() {
+    var ctx = document.getElementById("persons");
+		new Chart(ctx, {
+				type: 'pie',
+				labels: ["Femmes", "Hommes"],
+				data: {
+					datasets: [{
+						backgroundColor: [
+							"#eb5076",
+							"#fba540",
+						],
+						data: [70, 30,],
+						borderWidth: [0, 0]
+					}]
+				},
+			options: {
+			   legend: {
+				 position :"right",
+				 display: true,
+				    labels: {
+					  fontColor: '#585757',
+					  boxWidth:15
+				   }
+				}
+			   }
+		});
+  }
+
+  displayDistrict() {
+    var ctx = document.getElementById("regionchart");
+		new Chart(ctx, {
+				type: 'doughnut',
+				data: {
+					labels: ["Arrond 1", "Arrond 2", "Arrond 3", "Arrond 4"],
+					datasets: [{
+						backgroundColor: [
+							"#14abef",
+							"#02ba5a",
+							"#eb5076",
+							"#d13adf"
+						],
+						data: [13, 120, 11, 20],
+						borderWidth: [0, 0, 0, 0]
+					}]
+				},
+			options: {
+        title: {
+					display: false,
+				},
+				legend: {
+					display: false
+				},
+				responsive: true,
+			   }
+			});
   }
 
 }
