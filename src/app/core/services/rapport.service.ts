24f1f1a7caca9454fc7bc8fe8f74e642e52526b6
Vic-rider <tossouvincentsonagnon@gmail>
2021-02-22 22:13:33 +0100

    some update

--- src/app/core/services/rapport.service.ts
+++ src/app/core/services/rapport.service.ts
@@ -1,21 +1,16 @@
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { FileUpload } from '../models/fileUpload';
 import * as firebase from 'firebase';
 import 'firebase/storage';
 import { Rapport } from '../models/rapport';
-=======
-import { AngularFirestore } from '@angular/fire/firestore';
->>>>>>> 28ab407c312ca8e6895286174e9049fe8b5eafdb
 
 @Injectable({
   providedIn: 'root'
 })
 export class RapportService {
 
-<<<<<<< HEAD
   private basePath = '/rapport';
   rapportCollection: AngularFirestoreCollection<Rapport> = null;
 
@@ -93,16 +88,5 @@ export class RapportService {
   //   const storageRef = firebase.storage().ref();
   //   storageRef.child(`${this.basePath}/${name}`).delete();
   // }
-=======
-  constructor(private firedb: AngularFirestore) { }
-
-  addRapport(rapport){
-    return this.firedb.collection('rapport').add(rapport);
-  }
-
-  getRapport(){
-    return this.firedb.collection('rapport').snapshotChanges();
-  }
->>>>>>> 28ab407c312ca8e6895286174e9049fe8b5eafdb
 
 }
